52799ca4f1af0d42ed536f71383d7fee8522ea59
unknown <jreidmke@gmail.com>
2020-11-14 14:13:11 -0600

    8ball complete

--- src/8Ball.js
+++ src/8Ball.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
 import Answers from './Answers';
+import "./8Ball.css";
 
 const EightBall = (prop) => {
+    const randIdx = () => Math.floor(Math.random() * Answers.length);
 
+    const [answer, setAnswer] = useState('Give me a shake!');
+    const [color, setColor] = useState('white');
 
+    const shake = () => {
+        const idx = randIdx();
+        setAnswer(Answers[idx].msg)
+        setColor(Answers[idx].color);
+    };
 
+    const button = <button onClick={shake}>Shake it Up</button>
     return (
-        <div>
-            <h1>HEY</h1>
+        <div style={{'background-color': color}}>
+            <h1>{answer}</h1>
+            {button}
         </div>
     )
 }
