be248ac90b623684c62f335506ef3e424624faec
unknown <jreidmke@gmail.com>
2020-11-14 14:00:58 -0600

    basic starter code

---
+++ src/8Ball.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import Answers from './Answers';
+
+const EightBall = (prop) => {
+
+
+
+    return (
+        <div>
+            <h1>HEY</h1>
+        </div>
+    )
+}
+
+export default EightBall;
